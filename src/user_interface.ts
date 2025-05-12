5da0da7103f3292a1699385d4f97572f003a26fa
Fiona Johanna Weber <dev@fionajw.de>
2025-05-12 05:02:25 +0200

    remove no longer needed IDs from generated elements

--- src/user_interface.ts
+++ src/user_interface.ts
@@ -71,7 +71,6 @@ function createResultsTable(): [HTMLTableElement, HTMLElement] {
 	tr.appendChild(createTh("5%", "Actions"));
 	tr.appendChild(createTh("30%", "Notes"));
 	let tbody = document.createElement("tbody");
-	tbody.id="RecordResultTableBody";
 	table.appendChild(tbody);
 	return [table, tbody];
 }
@@ -79,7 +78,6 @@ function createResultsTable(): [HTMLTableElement, HTMLElement] {
 function setupUi(root: HTMLElement): UserInterface{
 
 	let canvas = document.createElement("canvas");
-	canvas.id = "canvas";
 	root.appendChild(canvas)
 	let volumeLabel = <HTMLLabelElement> document.createElement("label");
 	volumeLabel.innerHTML = "Volume-Threshold:";
@@ -91,23 +89,18 @@ function setupUi(root: HTMLElement): UserInterface{
 	volumeSelector.max = "255";
 	volumeSelector.step="1";
 	volumeSelector.value="32";
-	volumeSelector.id="VolumeThresholdSelector";
 	root.appendChild(volumeSelector);
 	root.appendChild(document.createTextNode("Main Frequency: "));
 	let freqOut = document.createElement("output");
-	freqOut.id = "freq-out";
 	root.appendChild(freqOut);
 	root.appendChild(document.createElement("br"));
 
 	let LanguageSelector = document.createElement("select");
-	LanguageSelector.id = "LanguageSelector";
 	root.appendChild(LanguageSelector);
 	let TextSelector = document.createElement("select");
-	TextSelector.id = "TextSelector";
 	root.appendChild(TextSelector);
 
 	let TextDisplay = document.createElement("blockquote");
-	TextDisplay.id="TextDisplay";
 	root.appendChild(TextDisplay);
 	root.appendChild(document.createElement("br"));
 
@@ -116,7 +109,6 @@ function setupUi(root: HTMLElement): UserInterface{
 	TargetFrequencySelectorLabel.innerHTML="Target Frequency in Hertz: ";
 	root.appendChild(TargetFrequencySelectorLabel)
 	let TargetFrequencySelector = document.createElement("input");
-	TargetFrequencySelector.id="TargetFrequencySelector";
 	TargetFrequencySelector.type="number";
 	TargetFrequencySelector.min="0";
 	TargetFrequencySelector.value="250";
@@ -124,16 +116,13 @@ function setupUi(root: HTMLElement): UserInterface{
 	root.appendChild(document.createElement("br"));
 
 	let PlayFrequencyButton=document.createElement("button");
-	PlayFrequencyButton.id="PlayFrequencyButton";
 	PlayFrequencyButton.type="button";
 	PlayFrequencyButton.innerHTML="▶️";
 	root.appendChild(PlayFrequencyButton);
 	let NoteSelector = document.createElement("div");
-	NoteSelector.id="NoteSelector";
 	root.appendChild(NoteSelector);
 	root.appendChild(document.createElement("br"));
 	let ToggleRecordButton = document.createElement("button");
-	ToggleRecordButton.id="ToggleRecordButton";
 	ToggleRecordButton.innerHTML="Start Recording";
 	root.appendChild(ToggleRecordButton);
 	let AutoPlaybackLabel = document.createElement("label");
@@ -141,7 +130,6 @@ function setupUi(root: HTMLElement): UserInterface{
 	AutoPlaybackLabel.innerHTML="Automatically play recording"
 	root.appendChild(AutoPlaybackLabel);
 	let AutoPlayback = document.createElement("input");
-	AutoPlayback.id="AutoPlayback";
 	AutoPlayback.type="checkbox";
 	root.appendChild(AutoPlayback);
 

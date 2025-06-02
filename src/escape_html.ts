d2723c3938f0ed512f299cc53790bfda3da20823
Fiona Johanna Weber <dev@fionajw.de>
2025-06-02 14:54:30 +0200

    further work on escaping

--- src/escape_html.ts
+++ src/escape_html.ts
@@ -1,14 +1,65 @@
 
+enum TagType {
+	Open,
+	Close,
+	OpenClose,
+	Raw
+}
+
+interface Tag {
+	name: string;
+	lang: string|null;
+	raw: string;
+	type: TagType;
+}
+
 export function escapeHTML(raw: string): string {
 	return new Option(raw).innerHTML;
 }
 
 
-
-function parseTag(raw: string): [string, string] {
-	const end = raw.indexOf('^>');
+function parseTag(raw: string): [Tag, string] {
+	const end = raw.indexOf('>');
 	if (end == -1) {
-		return [escapeHTML(raw), ""];
+		return [{
+			name: "",
+			lang: null,
+			raw: raw,
+			type: TagType.Raw
+		}, ""];
+	}
+	const rawTag = raw.slice(0, end);
+	const tail = raw.slice(end);
+	return [{
+		name: "",
+		lang: null,
+		raw: rawTag,
+		type: TagType.OpenClose
+	}, tail];
+}
+
+function handleTag(tag: Tag): string {
+	const acceptedTags = ["q", "emph", "strong", "p", "br", "blockquote", "span", "div"];
+	if ((tag.name == "") || !(tag.name in acceptedTags)) {
+		return escapeHTML(tag.raw);
+	}
+	switch(tag.type) {
+		case TagType.Open:
+			if (tag.lang != null) {
+				return "<" + tag.name + " lang=\"" + tag.name + "\">";
+			} else {
+				return "<" + tag.name + ">";
+			}
+		case TagType.Close:
+			return "</" + tag.name + ">";
+		case TagType.OpenClose:
+			if (tag.lang != null) {
+				return "<" + tag.name + " lang=\"" + tag.name + "\" />";
+			} else {
+				return "<" + tag.name + "/>";
+			}
+		case TagType.Raw:
+			return escapeHTML(tag.raw);
 	}
 }
 
@@ -17,7 +68,7 @@ export function filterSafeHTML(raw: string): string {
 	let specialStart = raw.search("/<|>|\"|'/");
 	while (specialStart != -1) {
 		ret += raw.slice(0, specialStart);
-		raw = raw.slize(specialStart);
+		raw = raw.slice(specialStart);
 		switch(raw[0]) {
 			case '"':
 				ret += "&quot;";
